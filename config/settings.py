745468453d3054bf043bbcf92153d540518675de
elahe <emotavalian@gmail.com>
2024-04-14 01:17:52 +0330

    custom user model created

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom user model config
+AUTH_USER_MODEL = "accounts.CustomUser"
