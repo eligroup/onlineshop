4882c6c75d28d6c7b8f0d6d11c1bd15f5de8b2b0
elahe <emotavalian@gmail.com>
2024-04-15 00:59:04 +0330

    test.py for home page and about us created

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_homepage_by_name(self):
+        response= self.client.get(reverse('home'))
+        self.assertEqual(response.status_code,200)
+
+    def test_homepage_by_url(self):
+        response=self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_by_template(self):
+        response=self.client.get(reverse("home"))
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_homepage_by_body(self):
+        response=self.client.get('/')
+        self.assertContains(response,"home page!!!")
+
+    def test_aboutus_by_name(self):
+        response=self.client.get(reverse('about_us'))
+        self.assertEqual(response.status_code,200)
+
+    def test_aboutus_by_template(self):
+        response=self.client.get('/about_us/')
+        self.assertTemplateUsed(response,'pages/about_us.html')
+
+    def test_aboutus_by_url(self):
+        response=self.client.get("/about_us/")
+        self.assertEqual(response.status_code,200)
+
+    def test_aboutus_by_body(self):
+        response=self.client.get(reverse('about_us'))
+        self.assertContains(response,"about us!!!")
+
+
+
+
+
