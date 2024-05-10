35a2fa4ebdb61ae473c032835cba8347adf63fc2
elahe <emotavalian@gmail.com>
2024-05-10 15:48:23 +0330

    tabular inline created and in product , comments will be shown

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import Product,Comment
 
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ['author','body','stars','active']
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title','price','active']
+    inlines = [
+        CommentInline,
+    ]
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
